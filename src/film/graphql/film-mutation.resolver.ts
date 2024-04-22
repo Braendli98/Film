397720554253041bafa1ccbbad8a53dec7b91c09
Anastasia Bologan <anastasia.bologan19@gmail.com>
2024-04-22 22:13:27 +0200

    Graphql/Rest-WriteController

---
+++ src/film/graphql/film-mutation.resolver.ts
@@ -0,0 +1,176 @@
+
+// eslint-disable-next-line max-classes-per-file
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { AuthGuard, Roles } from 'nest-keycloak-connect';
+import { IsInt, IsNumberString, Min } from 'class-validator';
+import { UseFilters, UseGuards, UseInterceptors } from '@nestjs/common';
+import { type Filmplakat } from '../entity/filmplakat.entity.js';
+import { type Film as Film } from '../entity/film.entity.js';
+import { FilmDTO as FilmDTO } from '../rest/filmDTO.entity.js';
+import { FilmWriteService as FilmWriteService } from '../service/film-write.service.js';
+import { HttpExceptionFilter } from './http-exception.filter.js';
+import { type IdInput } from './film-query.resolver.js';
+import { ResponseTimeInterceptor } from '../../logger/response-time.interceptor.js';
+import { type Titel } from '../entity/titel.entity.js';
+import { getLogger } from '../../logger/logger.js';
+
+// Authentifizierung und Autorisierung durch
+//  GraphQL Shield
+//      https://www.graphql-shield.com
+//      https://github.com/maticzav/graphql-shield
+//      https://github.com/nestjs/graphql/issues/92
+//      https://github.com/maticzav/graphql-shield/issues/213
+//  GraphQL AuthZ
+//      https://github.com/AstrumU/graphql-authz
+//      https://www.the-guild.dev/blog/graphql-authz
+
+export interface CreatePayload {
+    readonly id: number;
+}
+
+export interface UpdatePayload {
+    readonly version: number;
+}
+
+export class FilmUpdateDTO extends FilmDTO {
+    @IsNumberString()
+    readonly id!: string;
+
+    @IsInt()
+    @Min(0)
+    readonly version!: number;
+}
+@Resolver()
+// alternativ: globale Aktivierung der Guards https://docs.nestjs.com/security/authorization#basic-rbac-implementation
+@UseGuards(AuthGuard)
+@UseFilters(HttpExceptionFilter)
+@UseInterceptors(ResponseTimeInterceptor)
+export class FilmMutationResolver {
+    readonly #service: FilmWriteService;
+
+    readonly #logger = getLogger(FilmMutationResolver.name);
+
+    constructor(service: FilmWriteService) {
+        this.#service = service;
+    }
+
+    @Mutation()
+    @Roles({ roles: ['admin', 'user'] })
+    async create(@Args('input') filmDTO: FilmDTO) {
+        this.#logger.debug('create: filmDTO=%o', filmDTO);
+
+        const film = this.#filmDtoToFilm(filmDTO);
+        const id = await this.#service.create(film);
+        // TODO BadUserInputError
+        this.#logger.debug('createFilm: id=%d', id);
+        const payload: CreatePayload = { id };
+        return payload;
+    }
+
+    @Mutation()
+    @Roles({ roles: ['admin', 'user'] })
+    async update(@Args('input') filmDTO: FilmUpdateDTO) {
+        this.#logger.debug('update: film=%o', filmDTO);
+
+        const film = this.#filmUpdateDtoToFilm(filmDTO);
+        const versionStr = `"${filmDTO.version.toString()}"`;
+
+        const versionResult = await this.#service.update({
+            id: Number.parseInt(filmDTO.id, 10),
+            film: film,
+            version: versionStr,
+        });
+        // TODO BadUserInputError
+        this.#logger.debug('updateFilm: versionResult=%d', versionResult);
+        const payload: UpdatePayload = { version: versionResult };
+        return payload;
+    }
+
+    @Mutation()
+    @Roles({ roles: ['admin'] })
+    async delete(@Args() id: IdInput) {
+        const idStr = id.id;
+        this.#logger.debug('delete: id=%s', idStr);
+        const deletePerformed = await this.#service.delete(idStr);
+        this.#logger.debug('deleteFilm: deletePerformed=%s', deletePerformed);
+        return deletePerformed;
+    }
+
+    #filmDtoToFilm(filmDTO: FilmDTO): Film {
+        const titelDTO = filmDTO.titel;
+        const titel: Titel = {
+            id: undefined,
+            titel: titelDTO.titel,
+            beschreibung: titelDTO.untertitel,
+            film: undefined,
+        };
+        const filmplakate = filmDTO.filmplakate?.map((filmplakatDTO) => {
+            const filmplakat: Filmplakat = {
+                id: undefined,
+                beschriftung: filmplakatDTO.beschriftung,
+                contentType: filmplakatDTO.contentType,
+                film: undefined,
+            };
+            return filmplakat;
+        });
+        const film: Film = {
+            id: undefined,
+            version: undefined,
+            bewertung: filmDTO.bewertung,
+            genre: filmDTO.genre,
+            preis: filmDTO.preis,
+            datum: filmDTO.datum,
+            titel,
+            filmplakate,
+            erzeugt: new Date(),
+            aktualisiert: new Date(),
+        };
+
+        // Rueckwaertsverweis
+        film.titel!.film = film;
+        return film;
+    }
+
+    #filmUpdateDtoToFilm(filmDTO: FilmUpdateDTO): Film {
+        return {
+            id: undefined,
+            version: undefined,
+            bewertung: filmDTO.bewertung,
+            genre: filmDTO.genre,
+            preis: filmDTO.preis,
+            datum: filmDTO.datum,
+            titel: undefined,
+            filmplakate: undefined,
+            erzeugt: undefined,
+            aktualisiert: new Date(),
+        };
+    }
+
+    // #errorMsgCreateBuch(err: CreateError) {
+    //     switch (err.type) {
+    //         case 'IsbnExists': {
+    //             return `Die ISBN ${err.isbn} existiert bereits`;
+    //         }
+    //         default: {
+    //             return 'Unbekannter Fehler';
+    //         }
+    //     }
+    // }
+
+    // #errorMsgUpdateBuch(err: UpdateError) {
+    //     switch (err.type) {
+    //         case 'BuchNotExists': {
+    //             return `Es gibt kein Buch mit der ID ${err.id}`;
+    //         }
+    //         case 'VersionInvalid': {
+    //             return `"${err.version}" ist keine gueltige Versionsnummer`;
+    //         }
+    //         case 'VersionOutdated': {
+    //             return `Die Versionsnummer "${err.version}" ist nicht mehr aktuell`;
+    //         }
+    //         default: {
+    //             return 'Unbekannter Fehler';
+    //         }
+    //     }
+    // }
+}
---
+++ src/film/rest/film-write.controller.ts
@@ -0,0 +1,249 @@
+
+
+/**
+ * Das Modul besteht aus der Controller-Klasse für Schreiben an der REST-Schnittstelle.
+ * @packageDocumentation
+ */
+
+import {
+    ApiBadRequestResponse,
+    ApiBearerAuth,
+    ApiCreatedResponse,
+    ApiForbiddenResponse,
+    ApiHeader,
+    ApiNoContentResponse,
+    ApiOperation,
+    ApiPreconditionFailedResponse,
+    ApiResponse,
+    ApiTags,
+} from '@nestjs/swagger';
+import { AuthGuard, Roles } from 'nest-keycloak-connect';
+import {
+    Body,
+    Controller,
+    Delete,
+    Headers,
+    HttpCode,
+    HttpStatus,
+    Param,
+    Post,
+    Put,
+    Req,
+    Res,
+    UseGuards,
+    UseInterceptors,
+} from '@nestjs/common';
+import { FilmDTO, BuchDtoOhneRef as FilmDtoOhneRef } from './filmDTO.entity.js';
+import { Request, Response } from 'express';
+import { type Filmplakat } from '../entity/filmplakat.entity.js';
+import { type Buch as Film } from '../entity/buch.entity.js';
+import { BuchWriteService as FilmWriteService } from '../service/film-write.service.js';
+import { ResponseTimeInterceptor } from '../../logger/response-time.interceptor.js';
+import { type Titel } from '../entity/titel.entity.js';
+import { getBaseUri } from './getBaseUri.js';
+import { getLogger } from '../../logger/logger.js';
+import { paths } from '../../config/paths.js';
+
+const MSG_FORBIDDEN = 'Kein Token mit ausreichender Berechtigung vorhanden';
+/**
+ * Die Controller-Klasse für die Verwaltung von Bücher.
+ */
+@Controller(paths.rest)
+@UseGuards(AuthGuard)
+@UseInterceptors(ResponseTimeInterceptor)
+@ApiTags('Film REST-API')
+@ApiBearerAuth()
+export class FilmguideWriteController {
+    readonly #service: FilmWriteService;
+
+    readonly #logger = getLogger(FilmguideWriteController.name);
+
+    constructor(service: FilmWriteService) {
+        this.#service = service;
+    }
+
+    /**
+     * Ein neuer Film wird asynchron angelegt. Der neu anzulegender Film ist als
+     * JSON-Datensatz im Request-Objekt enthalten. Wenn es keine
+     * Verletzungen von Constraints gibt, wird der Statuscode `201` (`Created`)
+     * gesetzt und im Response-Header wird `Location` auf die URI so gesetzt,
+     * dass damit der neu angelegter Film abgerufen werden kann.
+     *
+     * Falls Constraints verletzt sind, wird der Statuscode `400` (`Bad Request`)
+     * gesetzt und genauso auch wenn der Titel (oder die ISBN-Nummer) //todo bereits
+     * existieren.
+     *
+     * @param filmDTO JSON-Daten für ein Film im Request-Body.
+     * @param res Leeres Response-Objekt von Express.
+     * @returns Leeres Promise-Objekt.
+     */
+    @Post()
+    @Roles({ roles: ['admin', 'user'] })
+    @ApiOperation({ summary: 'Ein neuer Film anlegen' })
+    @ApiCreatedResponse({ description: 'Erfolgreich neu angelegt' })
+    @ApiBadRequestResponse({ description: 'Fehlerhafte Filmdaten' })
+    @ApiForbiddenResponse({ description: MSG_FORBIDDEN })
+    async post(
+        @Body() filmDTO: FilmDTO,
+        @Req() req: Request,
+        @Res() res: Response,
+    ): Promise<Response> {
+        this.#logger.debug('post: filmDTO=%o', filmDTO);
+
+        const film = this.#filmDtoToFilm(filmDTO);
+        const id = await this.#service.create(film);
+
+        const location = `${getBaseUri(req)}/${id}`;
+        this.#logger.debug('post: location=%s', location);
+        return res.location(location).send();
+    }
+
+    /**
+     * Ein vorhandener Film wird asynchron aktualisiert.
+     *
+     * Im Request-Objekt von Express muss die ID des zu aktualisierenden Filmes
+     * als Pfad-Parameter enthalten sein. Außerdem muss im Rumpf der zu
+     * aktualisierender Film als JSON-Datensatz enthalten sein. Damit die
+     * Aktualisierung überhaupt durchgeführt werden kann, muss im Header
+     * `If-Match` auf die korrekte Version für optimistische Synchronisation
+     * gesetzt sein.
+     *
+     * Bei erfolgreicher Aktualisierung wird der Statuscode `204` (`No Content`)
+     * gesetzt und im Header auch `ETag` mit der neuen Version mitgeliefert.
+     *
+     * Falls die Versionsnummer fehlt, wird der Statuscode `428` (`Precondition
+     * required`) gesetzt; und falls sie nicht korrekt ist, der Statuscode `412`
+     * (`Precondition failed`). Falls Constraints verletzt sind, wird der
+     * Statuscode `400` (`Bad Request`) gesetzt und genauso auch wenn der neue
+     * Titel (oder die neue ISBN-Nummer) bereits existieren. //todo
+     *
+     * @param filmDTO Filmdaten im Body des Request-Objekts.
+     * @param id Pfad-Paramater für die ID.
+     * @param version Versionsnummer aus dem Header _If-Match_.
+     * @param res Leeres Response-Objekt von Express.
+     * @returns Leeres Promise-Objekt.
+     */
+    // eslint-disable-next-line max-params
+    @Put(':id')
+    @Roles({ roles: ['admin', 'user'] })
+    @HttpCode(HttpStatus.NO_CONTENT)
+    @ApiOperation({
+        summary: 'Ein vorhandener Film aktualisieren',
+        tags: ['Aktualisieren'],
+    })
+    @ApiHeader({
+        name: 'If-Match',
+        description: 'Header für optimistische Synchronisation',
+        required: false,
+    })
+    @ApiNoContentResponse({ description: 'Erfolgreich aktualisiert' })
+    @ApiBadRequestResponse({ description: 'Fehlerhafte Filmdaten' })
+    @ApiPreconditionFailedResponse({
+        description: 'Falsche Version im Header "If-Match"',
+    })
+    @ApiResponse({
+        status: HttpStatus.PRECONDITION_REQUIRED,
+        description: 'Header "If-Match" fehlt',
+    })
+    @ApiForbiddenResponse({ description: MSG_FORBIDDEN })
+    async put(
+        @Body() filmDTO: FilmDtoOhneRef,
+        @Param('id') id: number,
+        @Headers('If-Match') version: string | undefined,
+        @Res() res: Response,
+    ): Promise<Response> {
+        this.#logger.debug(
+            'put: id=%s, filmDTO=%o, version=%s',
+            id,
+            filmDTO,
+            version,
+        );
+
+        if (version === undefined) {
+            const msg = 'Header "If-Match" fehlt';
+            this.#logger.debug('put: msg=%s', msg);
+            return res
+                .status(HttpStatus.PRECONDITION_REQUIRED)
+                .set('Content-Type', 'application/json')
+                .send(msg);
+        }
+
+        const film = this.#filmDtoOhneRefToFilm(filmDTO);
+        const neueVersion = await this.#service.update({ id, film: film, version });
+        this.#logger.debug('put: version=%d', neueVersion);
+        return res.header('ETag', `"${neueVersion}"`).send();
+    }
+
+    /**
+     * Ein Film wird anhand seiner ID-gelöscht, die als Pfad-Parameter angegeben
+     * ist. Der zurückgelieferte Statuscode ist `204` (`No Content`).
+     *
+     * @param id Pfad-Paramater für die ID.
+     * @returns Leeres Promise-Objekt.
+     */
+    @Delete(':id')
+    @Roles({ roles: ['admin'] })
+    @HttpCode(HttpStatus.NO_CONTENT)
+    @ApiOperation({ summary: 'Film mit der ID löschen' })
+    @ApiNoContentResponse({
+        description: 'Der Film wurde gelöscht oder war nicht vorhanden',
+    })
+    @ApiForbiddenResponse({ description: MSG_FORBIDDEN })
+    async delete(@Param('id') id: number) {
+        this.#logger.debug('delete: id=%s', id);
+        await this.#service.delete(id);
+    }
+
+    #filmDtoToFilm(filmDTO: FilmDTO): Film {
+        const titelDTO = filmDTO.titel;
+        const titel: Titel = {
+            id: undefined,
+            titel: titelDTO.titel,
+            beschreibung: titelDTO.beschreibung,
+            film: undefined,
+        }; //todo
+        const filmplakate = filmDTO.filmplakate?.map((filmplakatDTO) => {
+            const filmplakat: Filmplakat = {
+                id: undefined,
+                beschriftung: filmplakatDTO.beschriftung,
+                contentType: filmplakatDTO.contentType,
+                film: undefined,
+            };
+            return filmplakat;
+        });
+        const film = {
+            id: undefined,
+            version: undefined,
+            bewertung: filmDTO.bewertung,
+            genre: filmDTO.genre,
+            preis: filmDTO.preis,
+            datum: filmDTO.datum,
+            titel,
+            filmplakate,
+            erzeugt: new Date(),
+            aktualisiert: new Date(),
+        };
+
+        // Rueckwaertsverweise
+        film.titel.film = film;
+        film.filmplakate?.forEach((filmplakat) => {
+            filmplakat.film = film;
+        });
+        return film;
+    }
+
+    #filmDtoOhneRefToFilm(filmDTO: FilmDtoOhneRef): Film {
+        return {
+            id: undefined,
+            version: undefined,
+            bewertung: filmDTO.bewertung,
+            genre: filmDTO.genre,
+            preis: filmDTO.preis,
+            datum: filmDTO.datum,
+            titel: undefined,
+            filmplakate: undefined,
+            erzeugt: undefined,
+            aktualisiert: new Date(),
+        };
+    }
+}

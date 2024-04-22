1755b94b9831d91c7151bde9e4dc3478f1a73802
Meryem Daoussi <meryem.daoussi@gmail.com>
2024-04-22 23:26:46 +0200

    config in src

---
+++ src/config/resources/db/sqlite/create.sql
@@ -0,0 +1,53 @@
+-- Copyright (C) 2023 - present Juergen Zimmermann, Hochschule Karlsruhe
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty ofs
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+-- https://docs.python.org/dev/library/sqlite3.html#sqlite3-cli
+-- sqlite3 film.sqlite
+
+-- https://sqlite.org/lang_createtable.html
+-- https://sqlite.org/stricttables.html ab 3.37.0
+-- https://sqlite.org/syntax/column-constraint.html
+-- https://sqlite.org/autoinc.html
+-- https://sqlite.org/stricttables.html: INT, INTEGER, REAL, TEXT
+-- https://sqlite.org/lang_createindex.html
+-- https://stackoverflow.com/questions/37619526/how-can-i-change-the-default-sqlite-timezone
+
+CREATE TABLE IF NOT EXISTS film (
+    id             INTEGER PRIMARY KEY AUTOINCREMENT,
+    version        INTEGER NOT NULL DEFAULT 0,
+    bewertung      INTEGER NOT NULL CHECK (bewertung >= 0 AND bewertung <= 5),
+    genre          TEXT,
+    preis          REAL,
+    datum          TEXT,
+    erzeugt        TEXT NOT NULL,
+    aktualisiert   TEXT NOT NULL
+);
+-- CREATE INDEX IF NOT EXISTS film_isbn_idx ON film(isbn);
+
+CREATE TABLE IF NOT EXISTS titel (
+    id            INTEGER PRIMARY KEY AUTOINCREMENT,
+    titel         TEXT NOT NULL,
+    beschreibung  TEXT,
+    film_id       INTEGER NOT NULL UNIQUE REFERENCES film
+);
+
+
+CREATE TABLE IF NOT EXISTS plakat (
+    id              INTEGER PRIMARY KEY AUTOINCREMENT,
+    beschriftung    TEXT NOT NULL,
+    content_type    TEXT NOT NULL,
+    film_id         INTEGER NOT NULL REFERENCES film
+);
+CREATE INDEX IF NOT EXISTS plakat_film_id_idx ON plakat(film_id);
---
+++ src/config/resources/db/sqlite/insert.sql
@@ -0,0 +1,64 @@
+-- Copyright (C) 2023 - present Juergen Zimmermann, Hochschule Karlsruhe
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PgenreICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+-- "Konzeption und Realisierung eines aktiven Datenbanksystems"
+-- "Verteilte Komponenten und Datenbankanbindung"
+-- "Design Patterns"
+-- "Freiburger Chorfilm"
+-- "Maschinelle Lernverfahren zur Behandlung von Bonitätsrisiken im Mobilfunkgeschäft"
+-- "Software Pioneers"
+
+INSERT INTO film(id, version, bewertung, genre, preis, datum, erzeugt, aktualisiert) VALUES
+    (1,0,4,'KOMOEDIE',11.1,'2022-02-01','2022-02-01 00:00:00','2022-02-01 00:00:00');
+INSERT INTO film(id, version, bewertung, genre, preis, datum, erzeugt, aktualisiert) VALUES
+    (20,0,2,'ACTION',22.2,'2022-02-02','2022-02-02 00:00:00','2022-02-02 00:00:00');
+INSERT INTO film(id, version, bewertung, genre, preis, datum, erzeugt, aktualisiert) VALUES
+    (30,0,3,'KOMOEDIE',33.3,'2022-02-03','2022-02-03 00:00:00','2022-02-03 00:00:00');
+INSERT INTO film(id, version, bewertung, genre, preis, datum, erzeugt, aktualisiert) VALUES
+    (40,0,4,'KOMOEDIE',44.4,'2022-02-04','2022-02-04 00:00:00','2022-02-04 00:00:00');
+INSERT INTO film(id, version, bewertung, genre, preis, datum, erzeugt, aktualisiert) VALUES
+    (50,0,2,'FANTASY',55.5,'2022-02-05','2022-02-05 00:00:00','2022-02-05 00:00:00');
+INSERT INTO film(id, version, bewertung, genre, preis, datum, erzeugt, aktualisiert) VALUES
+    (60,0,1,'ACTION',66.6,'2022-02-06','2022-02-06 00:00:00','2022-02-06 00:00:00');
+
+INSERT INTO titel(id, titel, beschreibung, film_id) VALUES
+    (1,'Alpha','alpha',1);
+INSERT INTO titel(id, titel, beschreibung, film_id) VALUES
+    (20,'Beta',null,20);
+INSERT INTO titel(id, titel, beschreibung, film_id) VALUES
+    (30,'Gamma','gamma',30);
+INSERT INTO titel(id, titel, beschreibung, film_id) VALUES
+    (40,'Delta','delta',40);
+INSERT INTO titel(id, titel, beschreibung, film_id) VALUES
+    (50,'Epsilon','epsilon',50);
+INSERT INTO titel(id, titel, beschreibung, film_id) VALUES
+    (60,'Phi','phi',60);
+
+INSERT INTO plakat(id, beschriftung, content_type, film_id) VALUES
+    (1,'Abb. 1','img/png',1);
+INSERT INTO plakat(id, beschriftung, content_type, film_id) VALUES
+    (20,'Abb. 1','img/png',20);
+INSERT INTO plakat(id, beschriftung, content_type, film_id) VALUES
+    (21,'Abb. 2','img/png',20);
+INSERT INTO plakat(id, beschriftung, content_type, film_id) VALUES
+    (30,'Abb. 1','img/png',30);
+INSERT INTO plakat(id, beschriftung, content_type, film_id) VALUES
+    (31,'Abb. 2','img/png',30);
+INSERT INTO plakat(id, beschriftung, content_type, film_id) VALUES
+    (40,'Abb. 1','img/png',40);
+INSERT INTO plakat(id, beschriftung, content_type, film_id) VALUES
+    (50,'Abb. 1','img/png',50);
+INSERT INTO plakat(id, beschriftung, content_type, film_id) VALUES
+    (60,'Abb. 1','img/png',60);

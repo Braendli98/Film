adc1cc9bf5612e42fd49de311cb0f41622643c73
Meryem Daoussi <meryem.daoussi@gmail.com>
2024-04-25 14:24:22 +0200

    db in .extras

---
+++ .extras/db/oracle/sql/create-schema-film.sql
@@ -0,0 +1,7 @@
+-- SELECT sys_context('userenv', 'current_schema') FROM dual;
+
+-- https://docs.oracle.com/en/database/oracle/oracle-database/23/sqlrf/CREATE-SCHEMA.html
+-- Schema-Name wird auf den Namen des DB-Users gesetzt
+CREATE SCHEMA AUTHORIZATION film;
+
+exit
